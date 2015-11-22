527034bae2cdd8b8dc78649faf92638549283fad
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2015-11-22 16:12:13 +0100

    Introduce a variable for the main directory

--- launch-kvm.sh
+++ launch-kvm.sh
@@ -9,10 +9,11 @@ export QEMU_AUDIO_DAC_VOICES=2
 
 # tap qemu
 # version smp
-kvm -smp 4 -initrd /home/paulo/xen/initrd-2.4.7-10smp.img -kernel /home/paulo/xen/vmlinuz-2.4.7-10smp -hda /home/paulo/xen/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55 -net tap,vlan=0,ifname=tap0  -soundhw es1370 -cdrom /dev/cdrom  -full-screen  &
+ROOT_DIR=/home/paulo/xen
+kvm -smp 4 -initrd $ROOT_DIR/initrd-2.4.7-10smp.img -kernel $ROOT_DIR/vmlinuz-2.4.7-10smp -hda $ROOT_DIR/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55 -net tap,vlan=0,ifname=tap0  -soundhw es1370 -cdrom /dev/cdrom  -full-screen  &
 # ok
-#kvm -smp 4 -initrd /home/paulo/xen/initrd-2.4.7-10.img -kernel /home/paulo/xen/vmlinuz-2.4.7-10 -hda /home/paulo/xen/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55 -net tap,vlan=0,ifname=tap0  -soundhw es1370 -cdrom /dev/cdrom  -full-screen  &
-#kvm -smp 4 -initrd /home/paulo/xen/initrd-2.4.7-10.img -kernel /home/paulo/xen/vmlinuz-2.4.7-10 -hda /home/paulo/xen/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55  -net tap,vlan=0,ifname=tap0  -soundhw es1370 -device lsi -drive if=none,file=/dev/sr0,id=sr0 -device scsi-block,drive=sr0 &
+#kvm -smp 4 -initrd $ROOT_DIR/initrd-2.4.7-10.img -kernel $ROOT_DIR/vmlinuz-2.4.7-10 -hda $ROOT_DIR/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55 -net tap,vlan=0,ifname=tap0  -soundhw es1370 -cdrom /dev/cdrom  -full-screen  &
+#kvm -smp 4 -initrd $ROOT_DIR/initrd-2.4.7-10.img -kernel $ROOT_DIR/vmlinuz-2.4.7-10 -hda $ROOT_DIR/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55  -net tap,vlan=0,ifname=tap0  -soundhw es1370 -device lsi -drive if=none,file=/dev/sr0,id=sr0 -device scsi-block,drive=sr0 &
 # -full-screen
 # -vga std
 # -display sdl 
@@ -21,7 +22,7 @@ kvm -smp 4 -initrd /home/paulo/xen/initrd-2.4.7-10smp.img -kernel /home/paulo/xe
 
 
 # nat qemu
-#kvm -initrd /home/paulo/xen/initrd-2.4.7-10.img -kernel /home/paulo/xen/vmlinuz-2.4.7-10 -hda /home/paulo/xen/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55 -net user,vlan=0 -redir tcp:2222:10.0.2.1:22  -redir tcp:139:10.0.2.1:139 -redir udp:137:10.0.2.1:137 -redir udp:138:10.0.2.1:138   -redir tcp:80:10.0.2.1:80  -soundhw es1370 -cdrom /dev/cdrom -monitor stdio -full-screen &
+#kvm -initrd $ROOT_DIR/initrd-2.4.7-10.img -kernel $ROOT_DIR/vmlinuz-2.4.7-10 -hda $ROOT_DIR/paulo -append "root=/dev/hda1" -m 1024 -net nic,model=ne2k_pci,vlan=0,macaddr=00:13:D3:14:F5:55 -net user,vlan=0 -redir tcp:2222:10.0.2.1:22  -redir tcp:139:10.0.2.1:139 -redir udp:137:10.0.2.1:137 -redir udp:138:10.0.2.1:138   -redir tcp:80:10.0.2.1:80  -soundhw es1370 -cdrom /dev/cdrom -monitor stdio -full-screen &
 
 ./set-sound.sh
 
